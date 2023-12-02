e313f97d210f15dba0307438eb429fb87e04c8bd
consultores.brtech <consultores.brtech@mcbauchemie.com>
2023-12-02 14:18:01 -0300

    24 finish tela login

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
      loginCard = true
+
+     loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+     })
+
+     signUpForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+     })
+
+     constructor(private formBuilder : FormBuilder) {}
+
+     onSubmitLoginForm() : void {
+       console.log('DADOS DO FORM LOGIN', this.loginForm.value)
+     }
+
+     onSubmitSignupForm() : void {
+      console.log('DADOS DO FORM LOGIN', this.signUpForm.value)
+    }
 }
