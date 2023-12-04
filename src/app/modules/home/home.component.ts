fdde423bb818e9c330cbdf0188d7b200c0086dbb
consultores.brtech <consultores.brtech@mcbauchemie.com>
2023-12-04 15:47:30 -0300

    TOAST MOD implementado

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,3 +1,8 @@
+import { Message, MessageService } from 'primeng/api';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthRequest } from './../../models/interfaces/user/auth/AuthRequest';
+import { SignupUserRequest } from './../../models/interfaces/user/SignupUserRequest';
+import { UserService } from './../../services/user/user.service';
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 
@@ -7,26 +12,86 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-     loginCard = true
+  loginCard = true
 
-     loginForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]]
-     })
+  loginForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  })
 
-     signUpForm = this.formBuilder.group({
-      name: ['', [Validators.required]],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]]
-     })
+  signUpForm = this.formBuilder.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  })
 
-     constructor(private formBuilder : FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private cookieService: CookieService,
+    private messageService : MessageService
+    ) { }
 
-     onSubmitLoginForm() : void {
-       console.log('DADOS DO FORM LOGIN', this.loginForm.value)
-     }
+  onSubmitLoginForm(): void {
+    //console.log('DADOS DO FORM LOGIN', this.loginForm.value)
 
-     onSubmitSignupForm() : void {
-      console.log('DADOS DO FORM LOGIN', this.signUpForm.value)
+    if (this.loginForm.value && this.loginForm.valid) {
+      this.userService.authUser(this.loginForm.value as AuthRequest)
+      .subscribe({
+        next : (response) => {
+          if(response){
+            this.cookieService.set('USER_INFO', response?.token)
+            this.loginForm.reset()
+            this.messageService.add({
+              severity : 'success',
+              summary: 'Sucesso',
+              detail: `Bem vindo de volta ${response?.name}.`,
+              life: 2000
+            })
+          }
+        }, error : (err) => {
+
+          this.messageService.add({
+            severity : 'success',
+            summary: 'Sucesso',
+            detail: `Usuário criado com sucesso.`,
+            life: 2000
+          })
+        }
+      })
+
     }
+  }
+
+  onSubmitSignupForm(): void {
+    // console.log('DADOS DO FORM LOGIN', this.signUpForm.value)
+    if (this.signUpForm.value && this.signUpForm.valid) {
+      this.userService.signupUser(this.signUpForm.value as SignupUserRequest)
+        .subscribe({
+          next: (response) => {
+            if (response) {
+              console.log("response", response)
+              this.signUpForm.reset()
+              this.loginCard = true
+
+
+              this.messageService.add({
+                severity : 'success',
+                summary: 'Sucesso',
+                detail: `Usuário criado com sucesso ${response?.name}.`,
+                life: 2000
+              })
+            }
+          },
+          error: (err) => {
+            this.messageService.add({
+              severity : 'success',
+              summary: 'Sucesso',
+              detail: `Erro ao criar usuário.`,
+              life: 2000
+            })
+          }
+        })
+    }
+  }
 }
