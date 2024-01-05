2ea1f8d01732500b5b16b4d7c8833aff30ef7dfa
consultores.brtech <consultores.brtech@mcbauchemie.com>
2024-01-05 09:04:45 -0300

    editando categorias

--- src/app/modules/categories/components/category-form/category-form.component.ts
+++ src/app/modules/categories/components/category-form/category-form.component.ts
@@ -34,6 +34,64 @@ export class CategoryFormComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
+    this.categoryAction = this.ref.data;
+
+    if(this.categoryAction.event.action === this.editCategoryAction && this.categoryAction.event.categoryName !== null || undefined){
+      this.setCategoryName(this.categoryAction.event.categoryName as string)
+    }
+  }
+
+  setCategoryName(categoryName : string): void {
+    if(categoryName){
+      this.categoryForm.setValue({
+        name: categoryName
+      })
+    }
+  }
+
+  handleSubmitEditCategory(){
+    if(this.categoryForm?.value && this.categoryForm.valid && this.categoryAction.event.id){
+      const  requestEditCategory: { name : string , category_id : string} = {
+        name : this.categoryForm.value.name as string,
+        category_id : this.categoryAction.event.id
+      }
+
+      this.categoriesService.editCategoryName( requestEditCategory)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next : () => {
+          this.categoryForm.reset()
+
+          this.messageService.add({
+            severity : 'success',
+            summary: 'Sucesso',
+            detail : 'Categoria editada com sucesso',
+            life : 2000
+
+          })
+
+        }, error : (err) => {
+          console.log(err)
+
+          this.messageService.add({
+            severity : 'error',
+            summary: 'Erro',
+            detail : 'Categoria editada com erro',
+            life : 2000
+
+          })
+        }
+      })
+    }
+  }
+
+  handleSubmitCategoryAction() : void {
+
+    if(this.categoryAction.event.action === this.addCategoryAction){
+      this.handleSubmitAddCategory()
+    }else if(this.categoryAction.event.action === this.editCategoryAction){
+      this.handleSubmitEditCategory()
+    }
 
   }
 
